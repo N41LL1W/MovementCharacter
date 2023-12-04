1cd19423f082eb98280541ae3de57c063c7f2cd0
N41LL1W <willian.play.cel@gmail.com>
2023-12-04 09:11:48 -0300

    Basic Move

--- Assets/Scripts/MoveController.cs
+++ Assets/Scripts/MoveController.cs
@@ -10,7 +10,8 @@ public class MoveController : MonoBehaviour
     CharacterController _characterController;
     Vector3 _direction;
 
-    [SerializeField] float speed;
+    [SerializeField] float rotationSpeed = 10.0f; // Nova variável para controlar a velocidade de rotação
+    [SerializeField] float speed = 5.0f;
 
     private void Awake()
     {
@@ -19,12 +20,20 @@ public class MoveController : MonoBehaviour
 
     private void Update()
     {
+        if (_input.sqrMagnitude == 0) return;
+
+        // Rotação
+        var targetRotation = Mathf.Atan2(_direction.x, _direction.y) * Mathf.Rad2Deg;
+        var rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0.0f, targetRotation, 0.0f), rotationSpeed * Time.deltaTime);
+        transform.rotation = rotation;
+
+        // Movimentação
         _characterController.Move(_direction * speed * Time.deltaTime);
     }
 
     public void Move(InputAction.CallbackContext context)
     {
         _input = context.ReadValue<Vector2>();
-        _direction = new Vector3(_input.x, 0.0f, _input.y);
+        _direction = new Vector3(_input.x, 0.0f, _input.y).normalized;
     }
 }
